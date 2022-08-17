eef221fe194b38318fb967c6163e104a05183442
RJ DiNardi <rdinardi@bandwidth.com>
2022-08-17 14:38:24 -0400

    debug

--- index.js
+++ index.js
@@ -52,6 +52,8 @@ async function authorizeIngressFromAnotherSecurityGroup(ec2, securityGroup, secu
     ]
   };
 
+  console.log(params);
+
   await ec2.authorizeSecurityGroupIngress(params, function(err, data) {
     if (err) console.log(err, err.stack); // an error occurred
     else {
